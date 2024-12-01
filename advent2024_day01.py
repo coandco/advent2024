142f36c1d48f16c272603e046d5401accf25ad24
Clint Olson <coandco@gmail.com>
2024-11-30 21:46:10 -0800

    Even more futzing with day 1

--- advent2024_day01.py
+++ advent2024_day01.py
@@ -6,7 +6,7 @@ import time
 
 def main():
     data = [line.split() for line in read_data().splitlines()]
-    left, right = sorted([int(x[0]) for x in data]), sorted([int(x[1]) for x in data])
+    left, right = sorted(int(x[0]) for x in data), sorted(int(x[1]) for x in data)
     print(f"Part one: {sum(abs(l - r) for l, r in zip(left, right))}")
     counts = Counter(right)
     print(f"Part two: {sum(item * counts[item] for item in left)}")
