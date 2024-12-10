8b7aa372f57324cb354f75f6e3bc18539d1cf156
Clint Olson <coandco@gmail.com>
2024-12-09 22:17:54 -0800

    Move scoring logic to main for day 10

--- advent2024_day10.py
+++ advent2024_day10.py
@@ -20,7 +20,7 @@ class Map:
                 if char == "0":
                     self.trailheads.add(loc)
 
-    def score_trails(self, trailhead: Coord) -> Tuple[int, int]:
+    def find_trails(self, trailhead: Coord) -> Set[Tuple[Coord, ...]]:
         found_trails = set()
         stack: Deque[Tuple[Coord, ...]] = deque([(trailhead,)])
         while stack:
@@ -31,14 +31,15 @@ class Map:
             for step in trail[-1].cardinal_neighbors():
                 if self.topo[step] == self.topo[trail[-1]] + 1:
                     stack.append(trail + (step,))
-        return len(set(x[-1] for x in found_trails)), len(found_trails)
+        return found_trails
 
 
 def main():
     topo_map = Map(read_data())
-    scores = [topo_map.score_trails(x) for x in topo_map.trailheads]
-    print(f"Part one: {sum(x[0] for x in scores)}")
-    print(f"Part two: {sum(x[1] for x in scores)}")
+    trails = [topo_map.find_trails(x) for x in topo_map.trailheads]
+    # Deduplicate peaks for part one
+    print(f"Part one: {sum(len(set(route[-1] for route in trail)) for trail in trails)}")
+    print(f"Part two: {sum(len(x) for x in trails)}")
 
 
 if __name__ == "__main__":
