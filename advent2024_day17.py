688c229ceaabf939373bbc64aa1a44c87c28f436
Clint Olson <coandco@gmail.com>
2024-12-21 13:52:26 -0800

    Clean up day 17 a bit

--- advent2024_day17.py
+++ advent2024_day17.py
@@ -1,10 +1,10 @@
-from typing import NamedTuple, Self, Optional, Dict, Tuple, List, Iterable
+import re
+import time
+from typing import Dict, Iterable, List, NamedTuple, Optional, Self, Tuple
 
 from utils import read_data
-import time
-import re
 
-DIGITS = re.compile(r'\d+')
+DIGITS = re.compile(r"\d+")
 
 
 OPCODES = [
@@ -23,7 +23,7 @@ OPCODES = [
     # bdv
     lambda state, arg: {"b": state.a // pow(2, state.combo_val(arg)), "pc": state.pc + 2},
     # cdv
-    lambda state, arg: {"c": state.a // pow(2, state.combo_val(arg)), "pc": state.pc + 2}
+    lambda state, arg: {"c": state.a // pow(2, state.combo_val(arg)), "pc": state.pc + 2},
 ]
 
 
@@ -32,11 +32,6 @@ class ProgramState(NamedTuple):
     b: int = 0
     c: int = 0
     pc: int = 0
-
-    @staticmethod
-    def from_input(raw: str):
-        a, b, c = DIGITS.findall(raw)
-        return ProgramState(a=int(a), b=int(b), c=int(c), pc=0)
 
     def combo_val(self, val: int) -> int:
         if val in (0, 1, 2, 3):
@@ -49,7 +44,7 @@ class ProgramState(NamedTuple):
         return ProgramState(**(self._asdict() | changed_vals))
 
     def execute(self, program: List[int]) -> Tuple[Self, Optional[int]]:
-        op, arg = program[self.pc:self.pc+2]
+        op, arg = program[self.pc : self.pc + 2]
         output = self.combo_val(arg) % 8 if op == 5 else None
         return self.changed_state(OPCODES[op](self, arg)), output
 
@@ -62,6 +57,7 @@ def run_program(state: ProgramState, program: List[int]) -> List[int]:
             all_output.append(output)
     return all_output
 
+
 def run_singleton(a: int, program: List[int]) -> int:
     state = ProgramState(a=a, b=0, c=0, pc=0)
     while 0 <= state.pc < len(program):
@@ -69,14 +65,15 @@ def run_singleton(a: int, program: List[int]) -> int:
         if output is not None:
             return output
 
+
 def calc_a(prev_a: int, program: List[int], index: Optional[int] = -1) -> Iterable[int]:
-    if index == -(len(program)+1):
+    if index == -(len(program) + 1):
         yield prev_a
         return
     new_a = prev_a << 3
     for i in range(8):
         if run_singleton(new_a + i, program) == program[index]:
-            yield from calc_a(new_a + i, program, index-1)
+            yield from calc_a(new_a + i, program, index - 1)
 
 
 def main():
